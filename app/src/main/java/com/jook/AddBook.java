ce5c065823aac034f5e0a61675578d5da2df1757
Yosef Carmeli <ybcarmeli@gmail.com>
2015-12-22 01:09:59 +0200

    תחילת פעולת הכפתור של הוספת ספר

--- app/src/main/java/com/jook/AddBook.java
+++ app/src/main/java/com/jook/AddBook.java
@@ -2,6 +2,9 @@ package com.jook;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.R;
 
@@ -11,5 +14,15 @@ public class AddBook extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_book);
+    }
+
+    public void CreateBook(View view) {
+        String Name=(((EditText)findViewById(R.id.get_book_name)).getText()).toString();
+        String Writer= (((EditText)findViewById(R.id.get_writer)).getText()).toString();
+        String Publisher= (((EditText)findViewById(R.id.get_publisher)).getText()).toString();
+        String PublishYear= (((EditText)findViewById(R.id.get_publish_year)).getText()).toString();
+        String Category= ((Spinner)findViewById(R.id.get_book_category)).getSelectedItem().toString();
+
+
     }
 }
