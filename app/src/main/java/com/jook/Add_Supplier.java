da2ee4450deb2b1ef6b5d1915e2e6fe35bbfe1cd
Yosef Carmeli <ybcarmeli@gmail.com>
2015-12-21 16:24:43 +0200

    ניסיון

--- app/src/main/java/com/jook/Add_Supplier.java
+++ app/src/main/java/com/jook/Add_Supplier.java
@@ -1,11 +1,24 @@
 package com.jook;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.R;
 
 public class Add_Supplier extends AppCompatActivity {
+
+    Intent MyIntent=getIntent();
+
+    String ID = MyIntent.getStringExtra(ConstValue.ID);
+    String PrivateNames= MyIntent.getStringExtra(ConstValue.PrivateName);
+    String FamilyNames= MyIntent.getStringExtra(ConstValue.FamilyName);
+    String City= MyIntent.getStringExtra(ConstValue.City);
+    String Street= MyIntent.getStringExtra(ConstValue.Street);
+    String Building= MyIntent.getStringExtra(ConstValue.Building);
+    String Email= MyIntent.getStringExtra(ConstValue.Email);
+    String Phone= MyIntent.getStringExtra(ConstValue.Phone);
+    String CellPhone= MyIntent.getStringExtra(ConstValue.CellPhone);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
