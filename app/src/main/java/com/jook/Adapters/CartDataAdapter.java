bd0dc58282be0cc0d027de8f7b0ad81324b9ca10
Yosef Carmeli <ybcarmeli@gmail.com>
2016-01-07 15:49:56 +0200

    עוד

---
+++ app/src/main/java/com/jook/Adapters/CartDataAdapter.java
@@ -0,0 +1,30 @@
+package com.jook.Adapters;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by יוסי on 06/01/2016.
+ */
+public class CartDataAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
